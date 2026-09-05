397c041c235f92ad966e6ae9e32ebd8103d739b6
Chief Keefe <schultz911@gmail.com>
2026-09-06 01:38:12 +0530

    Bug fix: Error 404 on configuration page.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     applicationId = "com.aistudio.spendtracker.spndtr"
     minSdk = 24
     targetSdk = 36
-    versionCode = 5
+    versionCode = 6
     versionName = "1.1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
